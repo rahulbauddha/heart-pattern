b3a1df20836e704a328fa88cc1ca3e66b698b656
rahulbauddha <rahulb22@navgurukul.org>
2022-10-02 10:49:05 +0530

    Add files via upload

---
+++ heartpattern.py
@@ -0,0 +1,7 @@
+for row in range(6):
+	for col in range(7):
+		if (row==0 and col !=0 and col !=3 and col !=6)or (row==1 and col!=1 and col !=2 and col !=4 and col !=5)or (row==2 and col !=1 and col !=2 and col !=3 and col !=4 and col !=5) or (row==3 and col !=0 and col !=2 and col !=3 and col!=4 and col !=6)or (row==4 and col !=0 and col !=1 and col !=3 and col !=5 and col !=6)or (row==5 and col !=0 and col !=1 and col !=2 and col !=4 and col !=5 and col !=6):
+			print("*",end=" ")
+		else:
+			print(end="  ")
+	print()
